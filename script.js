b90a49e323116160aa928ce59b14ceadda781b85
briangymnastio-dotcom <briangymnastio@gmail.com>
2026-03-18 10:18:59 +0700

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\n\nWe both tried. We both cared. But the universe just never gave us enough space to become “us.”\n\nI just wonder… if we had met at a different point in life, would the ending have been different?\n\nAnyway, I hope you're doing okay, even though we’re not in each other’s lives anymore.`;
+const message = `Hi Grace...
+I'm really sorry for falling asleep last night.
+Maaf ya kalau belakangan ini effort aku terasa kurang buat kamu.
+
+Aku tahu kamu lagi sibuk kuliah, and I want to be there for you more.
+I’ll try my best to do better next time.
+
+I just want you to know that I love you so much, Grace. 
+Semangat terus ya, Ice Cream! ❤️`;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
